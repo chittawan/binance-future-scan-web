0fbb7bd42ce3d3aece4c57dd87a6b7d0c3cc3aa2
Chittawan Risukhumal <Chittawan@TCCTM-30920.local>
2026-01-01 22:05:58 +0700

    fixed:update

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,6 @@
 import axios, { AxiosError } from 'axios';
 import type { TradingConfig, ApiResponse } from '../types/tradingConfig';
 import type {
-  ClientConfig,
   ClientConfigListResponse,
   ClientConfigCreateRequest,
   ClientConfigCreateResponse,
